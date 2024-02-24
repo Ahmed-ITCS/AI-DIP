bf13c57c407d7ca58566aa79828c3acaf523179e
Ahmed Khawar <ahmed@Ahmeds-MacBook-Pro.local>
2024-02-24 16:15:48 +0500

    made few models

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from student.models import SkillAssessment
+from student.models import User
 
 # Register your models here.
+admin.site.register(SkillAssessment)
+admin.site.register(User)
--- student/models.py
+++ student/models.py
@@ -1,3 +1,19 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)  # You should use more secure methods to store passwords
+    skills = ArrayField(models.CharField(max_length=100), blank=True)
+    assessment = ArrayField(models.CharField(max_length=100), blank=True)
+
+class SkillAssessment(models.Model):
+    id = models.AutoField(primary_key=True)
+    proficiency_score = models.IntegerField()
+    proficiency_level = models.CharField(max_length=50)
+    studentid = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Skill Assessment for {self.student.name}"
+    
